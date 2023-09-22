3cf96be76302f9a5d2c868824ce6c189c54b5a23
Pablo Mezzano <109251760+pablomezzano@users.noreply.github.com>
2023-09-22 11:17:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// Get references to the display and buttons
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".buttons button");
+
+// Add event listeners to the buttons
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonText = button.textContent;
+    const buttonValue = button.value;
+
+    // Handle different button types (numbers, operators, clear, calculate)
+    if (buttonText === "=") {
+      try {
+        let expression = display.value.replace(/×/g, "*").replace(/÷/g, "/"); // Reemplaza "×" con "*", Reemplaza "÷" con "/"
+        display.value = eval(expression);
+      } catch (error) {
+        display.value = "Error";
+      }
+    } else if (buttonText === "C") {
+      display.value = "";
+    } else if (buttonValue) {
+      // Reemplazar símbolos en el display
+      display.value +=
+        buttonValue === "add"
+          ? "+"
+          : buttonValue === "multiply"
+          ? "×"
+          : buttonValue === "divide"
+          ? "÷"
+          : buttonValue === "split"
+          ? "-"
+          : buttonValue;
+    } else {
+      display.value += buttonText;
+    }
+  });
+});
